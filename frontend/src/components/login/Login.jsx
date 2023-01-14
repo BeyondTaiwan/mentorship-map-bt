568fbaa15d759a51dbd90dca2fd922338e3d82db
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-01-14 01:40:53 +0000

    feat: add access code interface to login page

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -5,6 +5,10 @@ import GoogleLogin from 'react-google-login';
 import API from '../../api/Api.jsx';
 
 export default class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { accessCodeEntered: false, accessCodeError: true };
+    }
     
     onGoogleLoginSuccess = (response) => {
         const idToken = response.tokenId;
@@ -24,26 +28,61 @@ export default class Login extends React.Component{
     validateTokenAndObtainSession({data, idToken}) {
         API.login(idToken, data);
     }
+
+    validateAccessToken = () => {
+        let notValid = !(document.getElementById("access-code-input").value === "ABC123"); // call api function to validate here
+        this.setState({ accessCodeEntered: true, accessCodeError: notValid });
+    }
+
+
     render() {
         return (
-            <div className='page'>
-                <div className="split left">
-                    <div className = 'center'>
-                        <img alt="logo" src={logo} id="login-img"></img>
-                    </div>
+            <div className="page">
+                <div className="welcome-banner">
+                    <img className="logo-image" alt="logo" src={logo} id="login-img"></img>
+                    <p className="welcome-text">Welcome to the BT Mentorship Map!</p>
                 </div>
 
-                <div className="split right">
-                    <div className = "center">
-                        <h5>Welcome!</h5>
-                        <GoogleLogin
-                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}  // your Google app client ID
-                            buttonText="Sign in with Google"
-                            onSuccess={this.onGoogleLoginSuccess} // perform your user logic here
-                            onFailure={this.onGoogleLoginFailure} // handle errors here
-                            isSignedIn={true}
-                        />
-                    </div>
+                <div className="login-box">
+                    <p className="login-box-header">Log in to your account</p>
+
+                    <GoogleLogin
+                        className="login-box-google-button"
+                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}  // your Google app client ID
+                        buttonText="Continue with Google"
+                        onSuccess={this.onGoogleLoginSuccess} // perform your user logic here
+                        onFailure={this.onGoogleLoginFailure} // handle errors here
+                        isSignedIn={true}
+                    />
+                    
+                    <p className="login-box-guidance-text">OR</p>
+
+                    {(this.state.accessCodeError ? 
+                        <div>
+                            {(!this.state.accessCodeEntered ?
+                                <input className="login-box-input" id="access-code-input" type="text" placeholder="Enter access code" onSubmit={this.validateAccessToken}/>
+                                :
+                                <>
+                                    <input className="login-box-input login-box-input--error" id="access-code-input" type="text" placeholder="Enter access code" onSubmit={this.validateAccessToken}/>
+                                    <p className="login-box-input-hint">Invalid access code, try again.</p>
+                                </>
+                            )}
+                            <br></br>
+                            <button className="login-box-submit" onClick={this.validateAccessToken}>Continue</button>
+                        </div>
+                        :
+                        <div>
+                            <p className="login-box-header login-box-header--sign-up">Access code validated</p>
+                            <GoogleLogin
+                                className="login-box-google-button"
+                                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}  // your Google app client ID
+                                buttonText="Sign up with Google"
+                                onSuccess={this.onGoogleLoginSuccess} // perform your user logic here
+                                onFailure={this.onGoogleLoginFailure} // handle errors here
+                                isSignedIn={true}
+                            />
+                        </div>
+                    )}
                 </div>  
             </div>
         )
